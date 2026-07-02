b2cc3b0dde6f4618687d171374a5e72a73d4dd1a
Alexander Subbotin <112218640+ZZISST@users.noreply.github.com>
2026-07-02 19:37:39 +0300

    Add: is-anagram - submission-19

---
+++ Data Structures & Algorithms/is-anagram/submission-19.go
@@ -0,0 +1,20 @@
+func isAnagram(s string, t string) bool {
+	if len(s) != len(t) {
+		return false
+	}
+	counter := make(map[rune]int)
+
+	for _, ch := range s {
+		counter[ch]++
+	}
+
+	for _, ch := range t {
+		counter[ch]--
+		if counter[ch] < 0 {
+			return false
+		}
+	}
+
+	return true
+
+}
