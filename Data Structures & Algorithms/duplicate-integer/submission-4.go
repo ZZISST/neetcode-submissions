93675e74b3ce26f03326497c22f976fffb0f86d3
Alexander Subbotin <112218640+ZZISST@users.noreply.github.com>
2026-07-03 21:44:27 +0300

    Add: duplicate-integer - submission-4

---
+++ Data Structures & Algorithms/duplicate-integer/submission-4.go
@@ -0,0 +1,14 @@
+func hasDuplicate(nums []int) bool {
+	m := make(map[int]bool)
+
+	for _, v := range nums {
+
+		if _, ok := m[v]; ok {
+			return true
+		}
+		
+		m[v] = false
+
+	}
+	return false
+}
